f8b4fb43d7e9f5af37ad2176705ecfff4315b693
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-11-11 16:56:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var status1="";
+img="";
+objects=[];
+function preload(){
+    song=loadSound("music.mp3");
+}
+function setup(){
+canvas=createCanvas(380,380);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(380,380);
+video.hide();
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="status:detecting object";
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+status1=true;
+}
+function gotResult(error,results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+objects=results;
+}
+function draw(){
+image(video,0,0,380,380);
+if(status1!=""){
+    r=ramdom(255);
+    g=ramdom(255);
+    b=ramdom(255);
+    objectDetector.detect(video,gotResult);
+    for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="status:objects detected";
+        document.getElementById("number_objects").innerHTML="number of objects detected-"+objects.length;
+        fill(r,g,b);
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke(r,g,b);
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+if(objects[i].label!="person"){
+    song.play();
+}
+    }
+}
+}
